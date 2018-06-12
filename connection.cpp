a9e2a7361e9ef360846b93fa3b03922fa05669a3
Martin Bednar <wair92@Martins-MacBook-Pro.local>
2018-06-12 18:32:26 +0200

    Added login handling + response to client

---
+++ connection.cpp
@@ -0,0 +1,6 @@
+#include "connection.h"
+
+CONNECTION::CONNECTION()
+{
+
+}
---
+++ connection.h
@@ -0,0 +1,11 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+
+class CONNECTION
+{
+public:
+    CONNECTION();
+};
+
+#endif // CONNECTION_H
